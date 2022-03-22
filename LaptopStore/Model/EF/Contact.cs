7cbc0e17b9ef1ff74480121e3bf5c2b607370ac0
VuVanHungHD <vuhung03062000@gmail.com>
2022-03-22 19:48:02 +0700

    model,giao dien admin

---
+++ LaptopStore/Model/EF/Contact.cs
@@ -0,0 +1,21 @@
+namespace Model.EF
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Contact")]
+    public partial class Contact
+    {
+        public int ID { get; set; }
+
+        [Column(TypeName = "ntext")]
+        public string Content { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        public string Status { get; set; }
+    }
+}
